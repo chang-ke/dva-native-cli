a1f2daf2e3f9acc5837487a296bf28a58514277b
fruit-memory <315764194@qq.com>
2018-05-04 21:57:08 +0800

    修复复制二进制文件失败的bug

--- bin/dva-native.js
+++ bin/dva-native.js
@@ -1,31 +1,41 @@
 #! /usr/bin/env node
-const path = require("path");
-const fs = require("fs");
-const readline = require("readline");
-const help = require("./helper");
-const shell = require("shelljs");
-const colors = require("colors");
+const path = require('path');
+const fs = require('fs');
+const readline = require('readline');
+const help = require('./helper');
+const shell = require('shelljs');
+const colors = require('colors');
 const cwdPath = process.cwd();
-const package = require("../package.json");
-const templatePath = path.resolve(__dirname, "../template");
-const program = require("../utils/commander");
+const package = require('../package.json');
+const templatePath = path.resolve(__dirname, '../template');
+const program = require('../utils/commander');
+
+const Regx = (() => {
+  let regstr = '';
+  /**
+   * 二进制文件后缀，在rename的时候用来过滤
+   */
+  ['png', 'jpg', 'jpeg', 'jar', 'gif'].forEach(e => {
+    regstr += e + '|';
+  });
+  regstr = `\.(${regstr.slice(0, -1)})`
+  return new RegExp(regstr);
+})();
 
 function removeGit() {
-  shell.rm(
-    "-rf",
-    path.join(cwdPath, "react-native-dva-starter/.git"),
-    error => {
-      if (error) {
-        console.log(err);
-      }
+  shell.rm('-rf', path.join(cwdPath, 'react-native-dva-starter/.git'), error => {
+    if (error) {
+      console.log(err);
     }
-  );
+  });
 }
 
-function mkTargetDir() {
-  fs.mkdirSync(targetPath);
-}
-
+/**
+ *
+ *
+ * @param {string} path
+ * @returns {boolean} 路径是否存在
+ */
 function fsExistsSync(path) {
   try {
     fs.accessSync(path, fs.F_OK);
@@ -36,11 +46,7 @@ function fsExistsSync(path) {
 }
 
 function copyFile(templatePath, targetPath) {
-  // if (/\.(png|jar)/.test(targetPath)) {
-  //   fs.createReadStream(templatePath).pipe(fs.createWriteStream(targetPath));
-  // } else {
-    fs.writeFileSync(targetPath, fs.readFileSync(templatePath));
-  // }
+  fs.writeFileSync(targetPath, fs.readFileSync(templatePath));
 }
 
 function confirmOverride() {
@@ -49,17 +55,14 @@ function confirmOverride() {
     output: process.stdout
   });
   return new Promise((resolve, reject) => {
-    terminal.question(
-      "Floder is already exits, do you want override it ? yes/no ",
-      answer => {
-        if (answer === "yes" || answer === "y") {
-          resolve(true);
-          terminal.close();
-        } else {
-          process.exit();
-        }
+    terminal.question('Floder is already exits, do you want override it ? yes/no ', answer => {
+      if (answer === 'yes' || answer === 'y') {
+        resolve(true);
+        terminal.close();
+      } else {
+        process.exit();
       }
-    );
+    });
   });
 }
 
@@ -72,7 +75,7 @@ function copyTemplate(templatePath, targetPath) {
     subpaths.forEach(subpath => {
       const tarPath = path.resolve(targetPath, subpath);
       const tempPath = path.resolve(templatePath, subpath);
-      console.log("creating..." + tarPath);
+      console.log('creating...' + tarPath);
       if (fs.statSync(tempPath).isDirectory()) {
         copyTemplate(tempPath, tarPath);
       } else {
@@ -93,12 +96,12 @@ function renameProject(targetPath, projectName) {
       const subfile = path.resolve(targetPath, subpath);
       const newPath = subfile.replace(/DvaStarter/gim, projectName);
       if (!fs.statSync(subfile).isFile()) {
-        if (subpath !== "images") {
+        if (subpath !== 'images') {
           fs.renameSync(subfile, newPath);
           renameProject(newPath, projectName);
         }
       } else {
-        if (subfile !== "gradle-wrapper.jar") {
+        if (!Regx.test(subfile)) {
           fs.renameSync(subfile, newPath);
           fs.writeFileSync(
             newPath,
@@ -116,27 +119,23 @@ function renameProject(targetPath, projectName) {
 }
 
 function installDependencies(projectName) {
-  console.log("\ninstalling...");
-  if (
-    shell.exec(
-      `cd ${projectName} && npm install --registry=http://registry.cnpmjs.org`
-    ).code
-  ) {
+  console.log('\ninstalling...');
+  if (shell.exec(`cd ${projectName} && npm install --registry=http://registry.cnpmjs.org`).code) {
     if (shell.exec(`cd ${projectName} && npm install`).code) {
-      console.log(colors.red("install dependencies failed!"));
+      console.log(colors.red('install dependencies failed!'));
     } else {
       console.log(
         colors.yellow(
           [
             `Success! Created ${projectName} at ${cwdPath}.`,
-            "Inside that directory, you can run several commands and more:",
-            "  * npm start: Starts you project.",
-            "  * npm test: Run test.",
-            "We suggest that you begin by typing:",
+            'Inside that directory, you can run several commands and more:',
+            '  * npm start: Starts you project.',
+            '  * npm test: Run test.',
+            'We suggest that you begin by typing:',
             `  cd ${projectName}`,
-            "  npm start",
-            "Happy hacking!"
-          ].join("\n")
+            '  npm start',
+            'Happy hacking!'
+          ].join('\n')
         )
       );
     }
@@ -160,24 +159,21 @@ function backupDir(templatePath, backupPath) {
 }
 
 function removeBackup(backupPath) {
-  shell.rm("-rf", backupPath);
+  shell.rm('-rf', backupPath);
 }
 
 function newLatestProject(projectName) {
-  shell.exec(
-    `git clone https://github.com/nihgwu/react-native-dva-starter.git`,
-    error => {
-      if (!error) {
-        const tarPath = path.join(cwdPath, "react-native-dva-starter");
-        removeGit();
-        renameProject(tarPath, projectName);
-        fs.renameSync(tarPath, projectName);
-        installDependencies(projectName);
-      } else {
-        console.log(error);
-      }
+  shell.exec(`git clone https://github.com/nihgwu/react-native-dva-starter.git`, error => {
+    if (!error) {
+      const tarPath = path.join(cwdPath, 'react-native-dva-starter');
+      removeGit();
+      renameProject(tarPath, projectName);
+      fs.renameSync(tarPath, projectName);
+      installDependencies(projectName);
+    } else {
+      console.log(error);
     }
-  );
+  });
 }
 
 async function newProject(projectName) {
@@ -185,12 +181,12 @@ async function newProject(projectName) {
   const backupPath = `${cwdPath}/.${projectName}`;
   if (fsExistsSync(targetPath)) {
     if (await confirmOverride()) {
-      fs.renameSync(targetPath, backupPath);
+      //fs.renameSync(targetPath, backupPath);
       if (copyTemplate(templatePath, targetPath)) {
         renameProject(targetPath, projectName);
         installDependencies(projectName);
       }
-      removeBackup(backupPath);
+      //removeBackup(backupPath);
     }
   } else {
     if (copyTemplate(templatePath, targetPath)) {
@@ -201,18 +197,14 @@ async function newProject(projectName) {
 }
 
 try {
-  program.option("-h, --help", help).option("-v, --version", () => {
+  program.option('-h, --help', help).option('-v, --version', () => {
     console.log(package.version);
   });
-  program.command("new <project>").action(newProject);
-  program.command("git <project>").action(newLatestProject);
+  program.command('new <project>').action(newProject);
+  program.command('git <project>').action(newLatestProject);
   program.catch((...argvs) => {
     console.log(colors.red(`Do not use dva-native ${argvs}`));
-    console.log(
-      "Run " +
-        colors.blue("dva-native --help") +
-        " to get the Commands that dva-native-cli supports"
-    );
+    console.log('Run ' + colors.blue('dva-native --help') + ' to get the Commands that dva-native-cli supports');
   });
   program.parse(process.argv);
 } catch (error) {
